f33bb5f79c715d32775340d7bdca64174ad11ce3
Nick <trumpernickel@yahoo.com>
2017-04-11 20:35:05 -0500

    Initial commit

---
+++ likes.py
@@ -0,0 +1,21 @@
+def likes(strs):
+    formats = {
+        0: "no one likes this",
+        1: "{} likes this",
+        2: "{} and {} like this",
+        3: "{}, {} and {} like this",
+        4: "{}, {} and {num_others} like this"
+    }
+    return formats[min(len(strs), 4)].format(*strs, num_others=len(strs)-2)
+
+print(likes([]))
+print(likes(['Peter']))
+print(likes(['Jacob', 'Alex']))
+print(likes(['Max', 'John', 'Mark']))
+print(likes(['Alex', 'Jacob', 'Mark', 'Max']))
+
+# likes [] // must be "no one likes this"
+# likes ["Peter"] // must be "Peter likes this"
+# likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+# likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+# likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
