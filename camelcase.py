b425805603ac6a1569abf3c41725b7841a7a49e5
Nick <trumpernickel@yahoo.com>
2017-04-12 23:38:25 -0500

    added unit tests

--- camelcase.py
+++ camelcase.py
@@ -1,3 +1,5 @@
+import unittest
+
 def to_camel_case(text):
     words = text.replace('_', '-').split('-')
 
@@ -6,6 +8,17 @@ def to_camel_case(text):
     else:
         return ""
 
+class CamelTest(unittest.TestCase):
+    def testEmpty(self):
+        self.assertEqual(to_camel_case(""), "")
+
+    def testFirstUpper(self):
+        self.assertEqual(to_camel_case("I_Am_the_Champion"), "IAmTheChampion")
+
+    def testFirstLower(self):
+        self.assertEqual(to_camel_case("i-am-awesome"), "iAmAwesome")
 
 print(to_camel_case("the-stealth-warrior"))
 print(to_camel_case("The_Stealth_Warrior"))
+
+unittest.main()
