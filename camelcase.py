1677f5ef2935fdfdfeda570bda359405592e269f
Nick <trumpernickel@yahoo.com>
2017-04-13 22:13:27 -0500

    updated per pylint

--- camelcase.py
+++ camelcase.py
@@ -1,21 +1,24 @@
+from __future__ import print_function
+
 import unittest
+
 
 def to_camel_case(text):
     words = text.replace('_', '-').split('-')
 
     if words:
         return words[0] + "".join([str.capitalize(word) for word in words[1:]])
-    else:
-        return ""
+    return ""
+
 
 class CamelTest(unittest.TestCase):
-    def testEmpty(self):
+    def test_empty(self):
         self.assertEqual(to_camel_case(""), "")
 
-    def testFirstUpper(self):
+    def temp_first_upper(self):
         self.assertEqual(to_camel_case("I_Am_the_Champion"), "IAmTheChampion")
 
-    def testFirstLower(self):
+    def test_first_lower(self):
         self.assertEqual(to_camel_case("i-am-awesome"), "iAmAwesome")
 
 print(to_camel_case("the-stealth-warrior"))
